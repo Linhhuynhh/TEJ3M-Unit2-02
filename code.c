154c7b9df45057a58dbf275564fae9911b6c6a95
Linh Huynh <linh.huynh@mths.ca>
2025-02-19 09:50:46 -0500

    Create code.c

---
+++ code.c
@@ -0,0 +1,25 @@
+/*
+  Blink with variable
+  
+  Turns an LED on for one second, then off for one second, repeatedly.
+  BUt the internal increase each time by 1 more seconds
+
+  created on feb 2025
+  by Linh Huynh
+*/
+ 
+int blinkTime = 1000; //set variable to 1000
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN< OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  //turn the LED on (HIGH is the voltage level)
+  delay(blinkTime);                 //waiit for length of variable blinkTime
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+
+  blinkTime = blinkTime + 1000;    // add 1 second (or 1000) to variable blinkTime
+}
